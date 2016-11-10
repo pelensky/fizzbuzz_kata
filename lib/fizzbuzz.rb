a16e521945dc05f21f480084fcde7fae8d28f66d
Dan Pelensky <dan@pelensky.com>
2016-11-10 09:45:25 +0000

    working fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,20 +1,20 @@
 def divisible_by_3?(number)
-  number % 3 == 0
+  divisible_by?(number, 3)
 end
 
 def divisible_by_5?(number)
-  number % 5 == 0
+  divisible_by?(number, 5)
 end
 
 def divisible_by_3_and_5?(number)
-  number % 15 == 0
+  divisible_by?(number, 15)
 end
 
 def divisible_by?(number, divisor)
   number % divisor == 0
 end
 
-def fizzbuzz (number)
+def fizzbuzz(number)
   return 0 if number == 0
   return "fizzbuzz" if divisible_by_3_and_5?(number)
   return "fizz" if divisible_by_3?(number)
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,57 +1,67 @@
 require "fizzbuzz"
 
 describe "fizzbuzz" do
-  it "should test if a number is divisible by three" do
+
+it "tests if a number is divisible by 3" do
   expect(divisible_by_3?(3)).to eq true
-  end
+end
 
-  it "should test if a number is not divisible by three" do
+it "tests if a number is not divisible by 3" do
   expect(divisible_by_3?(1)).to eq false
-  end
+end
 
-  it "should test if a number is divisible by five" do
-    expect(divisible_by_5?(5)).to eq true
-  end
+it "tests if a number is divisible by 5" do
+  expect(divisible_by_5?(5)).to eq true
+end
 
-  it "should test if a number is not divisible by five" do
-    expect(divisible_by_5?(1)).to eq false
-  end
+it "tests if a number is not divisible by 5" do
+  expect(divisible_by_5?(1)).to eq false
+end
 
-  it "should test if a number is divisible by both three and five" do
-    expect(divisible_by_3_and_5?(15)).to eq true
-  end
+it "tests if a number is divisible by both 3 and 5" do
+  expect(divisible_by_3_and_5?(15)).to eq true
+end
 
-  it "should test if a number is not divisible by both three and five" do
-    expect(divisible_by_3_and_5?(1)).to eq false
-  end
+it "tests if a number is not divisible by both 3 and 5" do
+  expect(divisible_by_3_and_5?(1)).to eq false
+end
 
-  it "should test for arbitrary divisibility" do
-    expect(divisible_by?(1, 1)).to eq true
-  end
+it "checks of divisibility of any numbers" do
+  expect(divisible_by?(1, 1)).to eq true
+  expect(divisible_by?(4, 2)).to eq true
+  expect(divisible_by?(6, 3)). to eq true
+end
 
-  it "should test for non divisibility" do
-    expect(divisible_by?(3, 2)).to eq false
-  end
+it "checks if numbers aren't divisible by each other" do
+  expect(divisible_by?(5, 2)).to eq false
+  expect(divisible_by?(6, 4)). to eq false
+  expect(divisible_by?(7, 2)). to eq false
+end
 
-  it "should return fizz for multiples of three" do
-    expect(fizzbuzz(3)).to eq "fizz"
-  end
+it "returns fizz if divisible by 3" do
+  expect(fizzbuzz(3)).to eq "fizz"
+end
 
-  it "should return buzz for multiples of five" do
-    expect(fizzbuzz(5)).to eq "buzz"
-  end
+it "returns buzz if divisible by 5" do
+  expect(fizzbuzz(5)).to eq "buzz"
+end
 
-  it "should return fizzbuzz for multiples of three and five" do
-    expect(fizzbuzz(15)).to eq "fizzbuzz"
-  end
+it "returns fizzbuzz if divisible by 3 and 5" do
+  expect(fizzbuzz(15)). to eq "fizzbuzz"
+end
 
-  it "should return the number if it is not a multiple of five or three" do
-    expect(fizzbuzz(1)).to eq 1
-  end
+it "returns a number if that number is not divisible by 3 or 5" do
+  expect(fizzbuzz(1)).to eq 1
+end
 
-  it "should return zero if the number is zero" do
-    expect(fizzbuzz(0)).to eq 0
-  end
+it "returns 0 if the number is 0" do
+  expect(fizzbuzz(0)).to eq 0
+end
+
+
+
+
+
 
 
 end
